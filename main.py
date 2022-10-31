29e24b7f7b22c8a0ead3f0766e8438d01c05c6fd
RyzhovVictor <55699818+RyzhovVictor@users.noreply.github.com>
2022-10-31 15:03:56 +0300

    Finish

--- main.py
+++ main.py
@@ -1,34 +1,54 @@
 import os
 import time
-
-# import PySimpleGUI as sg
-#
-# sg.theme('DarkAmber')  # Add a touch of color
-# # All the stuff inside your window.
-# layout = [[sg.Text('Some text on Row 1')],
-#           [sg.Text('Хотите ли вы запустить таймер на выключения компьютера ?'), sg.InputText()],
-#           [sg.Text('Через сколько минут выключить компьютер ? '), sg.InputText()],
-#           [sg.Button('Ok'), sg.Button('Cancel')]]
-#
-#
-# # Create the Window
-# window = sg.Window('Sleep timer', layout)
-# # Event Loop to process "events" and get the "values" of the inputs
-# while True:
-#     event, values = window.read()
-#     if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
-#         break
-#     print('You entered ', values[0], values[1])
-#
-# window.close()
+import tkinter as tk
 
 
-shutdown = input("Хотите ли вы запустить таймер на выключения компьютера ?").lower()
-if shutdown == "да":
-    timer = float(input('Через сколько минут выключить компьютер ? '))
-    timer = timer * 60
+def shutdown_30_min():
+    root.withdraw()
+    timer = 1 * 60
     time.sleep(timer)
     shutdown_command = "shutdown /s /t 00"
     os.system(shutdown_command)
-if shutdown == "нет":
-    exit()
+
+
+def shutdown_60_min():
+    root.withdraw()
+    timer = 60 * 60
+    time.sleep(timer)
+    shutdown_command = "shutdown /s /t 00"
+    os.system(shutdown_command)
+
+
+def shutdown_90_min():
+    root.withdraw()
+    timer = 90 * 60
+    time.sleep(timer)
+    shutdown_command = "shutdown /s /t 00"
+    os.system(shutdown_command)
+
+
+def shutdown_120_min():
+    root.withdraw()
+    timer = 120 * 60
+    time.sleep(timer)
+    shutdown_command = "shutdown /s /t 00"
+    os.system(shutdown_command)
+
+
+root = tk.Tk()
+root['bg'] = 'seashell3'
+root.title('Таймер выключения компьютера')
+root.geometry('430x105+600+200')
+root.resizable(width=False, height=False)
+
+tk.Label(text='Через сколько минут выключить компьютер ? ', font=('Consalas', 13, 'bold'), bg='seashell3').pack()
+tk.Button(text='30 минут', height=2, width=9, font=('Consalas', 11, 'bold'), relief=tk.GROOVE, bd=6,
+          bg='SkyBlue1', command=shutdown_30_min).place(x=15, y=35)
+tk.Button(text='60 минут', height=2, width=9, font=('Consalas', 11, 'bold'), relief=tk.GROOVE, bd=6,
+          bg='SkyBlue1', command=shutdown_60_min).place(x=115, y=35)
+tk.Button(text='90 минут', height=2, width=9, font=('Consalas', 11, 'bold'), relief=tk.GROOVE, bd=6,
+          bg='SkyBlue1', command=shutdown_90_min).place(x=215, y=35)
+tk.Button(text='120 минут', height=2, width=9, font=('Consalas', 11, 'bold'), relief=tk.GROOVE, bd=6,
+          bg='SkyBlue1', command=shutdown_120_min).place(x=315, y=35)
+
+root.mainloop()
